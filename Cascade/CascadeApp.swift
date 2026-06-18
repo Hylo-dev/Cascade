d2b6925206f35ad8c962b6e199bee8592a1f2f9e
Eliomar Rodriguez <104672885+eliorodr2104@users.noreply.github.com>
2026-06-18 16:17:00 +0200

    Initial Commit

---
+++ Cascade/CascadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CascadeApp.swift
+//  Cascade
+//
+//  Created by Eliomar on 18/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CascadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
